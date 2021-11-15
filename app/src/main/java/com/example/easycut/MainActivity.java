6a53f8d2f86a7ff387007b302f1370eb3d32329f
daniel231195 <dansela123@gmail.com>
2021-11-15 16:21:10 +0200

    first commit

---
+++ app/src/main/java/com/example/easycut/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.easycut;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.window.SplashScreen;
+
+public class MainActivity extends AppCompatActivity {
+    Handler h= new Handler();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        h.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this , splash_screen.class );
+                startActivity(intent);
+                finish();
+            }
+        }, 5000);
+    }
+}
