0d5bb4cff30723e2628726f1b20237d0206b904c
daniel231195 <dansela123@gmail.com>
2021-11-15 16:43:38 +0200

    screen_login

--- app/src/main/java/com/example/easycut/splash_screen.java
+++ app/src/main/java/com/example/easycut/splash_screen.java
@@ -2,7 +2,10 @@ package com.example.easycut;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class splash_screen extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class splash_screen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        Button sign = (Button) findViewById(R.id.sign_in);
+        sign.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(splash_screen.this , login.class );
+                startActivity(intent);
+
+            }
+        });
     }
 }
