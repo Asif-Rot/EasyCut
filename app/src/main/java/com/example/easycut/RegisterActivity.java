3f509cf8f0a2668f2c2036be292d89da281dbb2f
daniel231195 <dansela123@gmail.com>
2021-11-29 12:41:28 +0200

    google services json

--- app/src/main/java/com/example/easycut/RegisterActivity.java
+++ app/src/main/java/com/example/easycut/RegisterActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
